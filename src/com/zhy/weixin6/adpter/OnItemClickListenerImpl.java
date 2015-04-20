b5bf852d68134d451585c48e373a5f95daa8cac9
1000ren <fanwei1207@163.com>
2015-04-20 11:17:09 +0800

    增加启动界面

--- src/com/zhy/weixin6/adpter/OnItemClickListenerImpl.java
+++ src/com/zhy/weixin6/adpter/OnItemClickListenerImpl.java
@@ -1,27 +1,27 @@
-package com.zhy.weixin6.adpter;
-
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-
-import com.zhy.weixin6.ui.DayTabFragment;
-
-public class OnItemClickListenerImpl implements OnItemClickListener {
-	
-	private CalendarAdapter adapter = null;
-	private DayTabFragment activity = null;
-	public OnItemClickListenerImpl(CalendarAdapter adapter, DayTabFragment activity) {
-		this.adapter = adapter;
-		this.activity = activity;
-	}	
-	
-	public void onItemClick(AdapterView<?> gridView, View view, int position, long id) {
-		if (activity.currList.get(position).isThisMonth() == false) {
-			return;
-		}
-		adapter.setSelectedPosition(position);  
-		adapter.notifyDataSetInvalidated(); 
-		activity.lastSelected = activity.currList.get(position).getDate();
-	}
-	
-}
+//package com.zhy.weixin6.adpter;
+//
+//import android.view.View;
+//import android.widget.AdapterView;
+//import android.widget.AdapterView.OnItemClickListener;
+//
+//import com.zhy.weixin6.ui.DayTabFragment;
+//
+//public class OnItemClickListenerImpl implements OnItemClickListener {
+//	
+//	private CalendarAdapter adapter = null;
+//	private DayTabFragment activity = null;
+//	public OnItemClickListenerImpl(CalendarAdapter adapter, DayTabFragment activity) {
+//		this.adapter = adapter;
+//		this.activity = activity;
+//	}	
+//	
+//	public void onItemClick(AdapterView<?> gridView, View view, int position, long id) {
+//		if (activity.currList.get(position).isThisMonth() == false) {
+//			return;
+//		}
+//		adapter.setSelectedPosition(position);  
+//		adapter.notifyDataSetInvalidated(); 
+//		activity.lastSelected = activity.currList.get(position).getDate();
+//	}
+//	
+//}
--- src/com/zhy/weixin6/ui/DayTabFragment.java
+++ src/com/zhy/weixin6/ui/DayTabFragment.java
@@ -1,62 +1,47 @@
 package com.zhy.weixin6.ui;
 
-import java.util.List;
-
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.view.PagerAdapter;
-import android.support.v4.view.ViewPager.OnPageChangeListener;
-import android.view.Gravity;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.GridView;
+import android.widget.SlidingDrawer;
+import android.widget.SlidingDrawer.OnDrawerOpenListener;
+import android.widget.SlidingDrawer.OnDrawerScrollListener;
 import android.widget.TextView;
 
-import com.zhy.weixin6.adpter.CalendarAdapter;
-import com.zhy.weixin6.adpter.OnItemClickListenerImpl;
-import com.zhy.weixin6.data.DateInfo;
-import com.zhy.weixin6.utils.DataUtils;
-import com.zhy.weixin6.utils.TimeUtils;
+import com.zhy.weixin6.AddPlanActivity;
+import com.zhy.weixin6.caledar.util.DateUtil;
+import com.zhy.weixin6.calendar.doim.CalendarViewBuilder;
+import com.zhy.weixin6.calendar.doim.CustomDate;
+import com.zhy.weixin6.calendar.widget.CalendarView;
+import com.zhy.weixin6.calendar.widget.CalendarView.CallBack;
+import com.zhy.weixin6.calendar.widget.CalendarViewPagerLisenter;
+import com.zhy.weixin6.calendar.widget.CircleTextView;
+import com.zhy.weixin6.calendar.widget.CustomViewPagerAdapter;
 
-public class DayTabFragment extends Fragment {
-	/**
-	 * 和viewpager相关变量
-	 * */
-	public MyViewPager viewPager = null;
-	public MyPagerAdapter pagerAdapter = null;
-	private int currPager = 500;
-	private TextView shader;
-
-	/**
-	 * 和日历gridview相关变量
-	 * */
-	private GridView gridView = null;
-	public CalendarAdapter adapter = null;
-	private GridView currentView = null;
-	public List<DateInfo> currList = null;
-	public List<DateInfo> list = null;
-	public int lastSelected = 0;
-
-	/**
-	 * 显示年月
-	 * */
-	public TextView showYearMonth = null;
-
-	/**
-	 * 第一个页面的年月
-	 * */
-	private int currentYear;
-	private int currentMonth;
-
-	/**
-	 * 收缩展开的面板
-	 * */
-	private Panel panel;
-
+public class DayTabFragment extends Fragment implements OnClickListener,CallBack{
+	private ViewPager viewPager;
+	private CalendarView[] views;
+	private TextView showYearView;
+	private TextView showMonthView;
+	private TextView showWeekView;
+	private TextView monthCalendarView;
+	private TextView weekCalendarView;
+	private CalendarViewBuilder builder = new CalendarViewBuilder();
+	private SlidingDrawer mSlidingDrawer;
+	private View mContentPager;
+	private CustomDate mClickDate;
+	private CircleTextView mNowCircleView;
+	private CircleTextView mAddCircleView;
+	public static final String MAIN_ACTIVITY_CLICK_DATE = "main_click_date";
+	
+	
 	Context mContext = null;
 
 	public DayTabFragment() {
@@ -67,8 +52,7 @@ public class DayTabFragment extends Fragment {
 			Bundle savedInstanceState) {
 		mContext = ((MainActivity) getActivity()).getContext();
 
-		View view = inflater.inflate(R.layout.frag_daytab, container, false);
-
+		View view = inflater.inflate(R.layout.activity_mdaytab, container, false);
 		return view;
 
 	}
@@ -76,140 +60,114 @@ public class DayTabFragment extends Fragment {
 	@Override
 	public void onViewCreated(View view, Bundle savedInstanceState) {
 		super.onViewCreated(view, savedInstanceState);
-		initData();
-		initView(view);
+		findViewbyId(view);
+	}
+	private void findViewbyId(View view) {
+		viewPager = (ViewPager) view.findViewById(R.id.viewpager);
+		showMonthView = (TextView)view.findViewById(R.id.show_month_view);
+		showYearView = (TextView)view.findViewById(R.id.show_year_view);
+		showWeekView = (TextView)view.findViewById(R.id.show_week_view);
+		views = builder.createMassCalendarViews(mContext, 5, this);
+		monthCalendarView = (TextView)view.findViewById(R.id.month_calendar_button);
+		weekCalendarView = (TextView)view.findViewById(R.id.week_calendar_button);
+		mContentPager = view.findViewById(R.id.contentPager);
+		mSlidingDrawer = (SlidingDrawer)view.findViewById(R.id.sildingDrawer);
+		mNowCircleView = (CircleTextView)view.findViewById(R.id.now_circle_view);
+		mAddCircleView = (CircleTextView)view.findViewById(R.id.add_circle_view);
+		monthCalendarView.setOnClickListener(this);
+		weekCalendarView.setOnClickListener(this);
+		mNowCircleView.setOnClickListener(this);
+		mAddCircleView.setOnClickListener(this);
+		setViewPager();
+		setOnDrawListener();
 	}
 
-	/**
-	 * 初始化view
-	 * */
-	private void initView(View view) {
-		viewPager = (MyViewPager) view.findViewById(R.id.viewpager);
-		shader = (TextView) view.findViewById(R.id.main_frame_shader);
-		// FW
-		panel = (Panel) view.findViewById(R.id.panel);
-		panel.setOpen(true, false);
-		shader.setVisibility(View.GONE);
-		pagerAdapter = new MyPagerAdapter();
-		viewPager.setAdapter(pagerAdapter);
-		viewPager.setCurrentItem(500);
-		viewPager.setPageMargin(0);
-		showYearMonth = (TextView) view.findViewById(R.id.main_year_month);
-		showYearMonth.setText(String.format("%04d年%02d月", currentYear,
-				currentMonth));
 
-		viewPager.setOnPageChangeListener(new OnPageChangeListener() {
-			public void onPageScrollStateChanged(int arg0) {
-				if (arg0 == 1) {
-					shader.setText("");
-					shader.setVisibility(View.VISIBLE);
-				}
-				if (arg0 == 0) {
-					currentView = (GridView) viewPager.findViewById(currPager);
-					if (currentView != null) {
-						adapter = (CalendarAdapter) currentView.getAdapter();
-						currList = adapter.getList();
-						int pos = DataUtils.getDayFlag(currList, lastSelected);
-						adapter.setSelectedPosition(pos);
-						adapter.notifyDataSetInvalidated();
-					}
-					shader.setVisibility(View.GONE);
-				}
+	private void setViewPager() {
+		CustomViewPagerAdapter<CalendarView> viewPagerAdapter = new CustomViewPagerAdapter<CalendarView>(views);
+		viewPager.setAdapter(viewPagerAdapter);
+		viewPager.setCurrentItem(498); 
+		viewPager.setOnPageChangeListener(new CalendarViewPagerLisenter(viewPagerAdapter));
+	}
+
+	private void setOnDrawListener() {
+		mSlidingDrawer.setOnDrawerOpenListener(new OnDrawerOpenListener() {
+			
+			@Override
+			public void onDrawerOpened() {
+				builder.swtichCalendarViewsStyle(CalendarView.WEEK_STYLE);
 			}
-
-			public void onPageScrolled(int arg0, float arg1, int arg2) {
+		});
+		mSlidingDrawer.setOnDrawerScrollListener(new OnDrawerScrollListener() {
+			
+			@Override
+			public void onScrollStarted() {
+				builder.swtichCalendarViewsStyle(CalendarView.MONTH_STYLE);
 			}
-
-			public void onPageSelected(int position) {
-				int year = TimeUtils.getTimeByPosition(position, currentYear,
-						currentMonth, "year");
-				int month = TimeUtils.getTimeByPosition(position, currentYear,
-						currentMonth, "month");
-				showYearMonth.setText(String.format("%04d年%02d月", year, month));
-				currPager = position;
-				shader.setText(showYearMonth.getText());
+			
+			@Override
+			public void onScrollEnded() {
 			}
 		});
 	}
 
-	/**
-	 * 初始化数据
-	 * */
-	private void initData() {
-		currentYear = TimeUtils.getCurrentYear();
-		currentMonth = TimeUtils.getCurrentMonth();
-		lastSelected = TimeUtils.getCurrentDay();
-		String formatDate = TimeUtils.getFormatDate(currentYear, currentMonth);
-		try {
-			list = TimeUtils.initCalendar(formatDate, currentMonth);
-		} catch (Exception e) {
-			// finish();
-		}
+
+ public void setShowDateViewText(int year ,int month){
+	 showYearView.setText(year+"");
+	 showMonthView.setText(month+"月");
+	 showWeekView.setText(DateUtil.weekName[DateUtil.getWeekDay()-1]);
+ }
+
+ 	@Override
+ 	public void onClick(View v) {
+ 		switch (v.getId()) {
+		case R.id.month_calendar_button:
+			swtichBackgroundForButton(true);
+			builder.swtichCalendarViewsStyle(CalendarView.MONTH_STYLE);
+ 			mSlidingDrawer.close();
+			break;
+		case R.id.week_calendar_button:
+			swtichBackgroundForButton(false);
+			mSlidingDrawer.open();
+			break;
+		case R.id.now_circle_view:
+			builder.backTodayCalendarViews();
+			break;
+		case R.id.add_circle_view:
+			Intent i = new Intent(mContext,AddPlanActivity.class);
+			Bundle mBundle = new Bundle();  
+	        mBundle.putSerializable(MAIN_ACTIVITY_CLICK_DATE,mClickDate);
+	        i.putExtras(mBundle);  
+			startActivity(i);
+//			mContext.overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left);
+ 		}
+ 	}
+ 	
+ 	private void swtichBackgroundForButton(boolean isMonth){
+ 		if(isMonth){
+ 			monthCalendarView.setBackgroundResource(R.drawable.press_left_text_bg);
+ 			weekCalendarView.setBackgroundColor(Color.TRANSPARENT);
+ 		}else{
+ 			weekCalendarView.setBackgroundResource(R.drawable.press_right_text_bg);
+ 			monthCalendarView.setBackgroundColor(Color.TRANSPARENT);
+ 		}
+ 	}
+
+
+	@Override
+	public void onMesureCellHeight(int cellSpace) {
+		mSlidingDrawer.getLayoutParams().height = mContentPager.getHeight() - cellSpace;
 	}
 
-	/**
-	 * 初始化日历的gridview
-	 * */
-	private GridView initCalendarView(int position) {
-		int year = TimeUtils.getTimeByPosition(position, currentYear,
-				currentMonth, "year");
-		int month = TimeUtils.getTimeByPosition(position, currentYear,
-				currentMonth, "month");
-		String formatDate = TimeUtils.getFormatDate(year, month);
-		try {
-			list = TimeUtils.initCalendar(formatDate, month);
-		} catch (Exception e) {
-			// finish();
-		}
-		gridView = new GridView(mContext);
-		adapter = new CalendarAdapter((MainActivity) getActivity(), list);
-		if (position == 500) {
-			currList = list;
-			int pos = DataUtils.getDayFlag(list, lastSelected);
-			adapter.setSelectedPosition(pos);
-		}
-		gridView.setAdapter(adapter);
-		gridView.setNumColumns(7);
-		gridView.setSelector(new ColorDrawable(Color.TRANSPARENT));
-		gridView.setGravity(Gravity.CENTER);
-		gridView.setOnItemClickListener(new OnItemClickListenerImpl(adapter,
-				this));
-		return gridView;
+	@Override
+	public void clickDate(CustomDate date) {
+		mClickDate = date;
+		//Toast.makeText(this, date.year+"-"+date.month+"-"+date.day, Toast.LENGTH_SHORT).show();
 	}
 
-	/**
-	 * viewpager的适配器，从第500页开始，最多支持0-1000页
-	 * */
-	private class MyPagerAdapter extends PagerAdapter {
-
-		@Override
-		public void setPrimaryItem(ViewGroup container, int position,
-				Object object) {
-			currentView = (GridView) object;
-			adapter = (CalendarAdapter) currentView.getAdapter();
-		}
-
-		@Override
-		public int getCount() {
-			return 1000;
-		}
-
-		@Override
-		public boolean isViewFromObject(View arg0, Object arg1) {
-			return arg0 == arg1;
-		}
-
-		@Override
-		public void destroyItem(ViewGroup container, int position, Object object) {
-			container.removeView((View) object);
-		}
-
-		@Override
-		public Object instantiateItem(ViewGroup container, int position) {
-			GridView gv = initCalendarView(position);
-			gv.setId(position);
-			container.addView(gv);
-			return gv;
-		}
+	@Override
+	public void changeDate(CustomDate date) {
+		setShowDateViewText(date.year,date.month);
 	}
+
 }
